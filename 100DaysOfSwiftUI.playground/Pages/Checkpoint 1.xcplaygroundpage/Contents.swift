f52510a3816d29f1e798f8cdd604cee0159c1003
Jared Pateman <jared.pateman@outlook.com>
2023-07-10 22:43:47 +0100

    Add checkpoint 1 solution

---
+++ 100DaysOfSwiftUI.playground/Pages/Checkpoint 1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,7 @@
+import SwiftUI
+
+// Checkpoint 1 - https://www.hackingwithswift.com/quick-start/beginners/checkpoint-1
+
+let celciusTemperature = 25.9
+var fahrenheitTemperature = (celciusTemperature * 9 / 5) + 32
+print("\(celciusTemperature)°C is equal to \(fahrenheitTemperature)°F")
