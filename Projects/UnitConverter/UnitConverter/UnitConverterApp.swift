00deaaaff30f320af19e6af979faedf6cabda2e3
Jared Pateman <jared.pateman@outlook.com>
2023-07-27 17:24:59 +0100

    Add initial project for unit converter challenge

---
+++ Projects/UnitConverter/UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Jared Pateman on 27/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
