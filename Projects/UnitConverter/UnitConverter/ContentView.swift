6e16082a5bc2dfedc6309385b3f9b95607e458bc
Jared Pateman <jared.pateman@outlook.com>
2023-07-27 20:43:44 +0100

    Add code for unit converter app from day 19

--- Projects/UnitConverter/UnitConverter/ContentView.swift
+++ Projects/UnitConverter/UnitConverter/ContentView.swift
@@ -8,14 +8,85 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var valueToConvert: Double = 20.0
+    @State private var selectedInputUnit: String = "meters"
+    @State private var selectedOutputUnit: String = "miles"
+    @FocusState private var conversionIsFocused: Bool
+
+    private let units: [String: UnitLength] = [
+        "millimeters": .millimeters,
+        "centimeters": .centimeters,
+        "meters": .meters,
+        "kilometers": .kilometers,
+        "feet": .feet,
+        "yards": .yards,
+        "miles": .miles,
+        "inches": .inches
+    ]
+
+    var convertedValue: Double {
+        guard
+            let inputUnitType = units[selectedInputUnit],
+            let outputUnitType = units[selectedOutputUnit]
+        else { return 0 }
+
+        let inputUnitAmount = Measurement(value: valueToConvert, unit: inputUnitType)
+        let convertedUnit = inputUnitAmount.converted(to: outputUnitType)
+
+        return convertedUnit.value
+    }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Unit", selection: $selectedInputUnit) {
+                        ForEach(Array(units.keys), id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                } header: {
+                    Text("Input unit")
+                }
+
+                Section {
+                    TextField("Amount to convert", value: $valueToConvert, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($conversionIsFocused)
+                } header: {
+                    Text("Input unit amount")
+                }
+
+                Section {
+                    Picker("Unit", selection: $selectedOutputUnit) {
+                        ForEach(Array(units.keys), id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                } header: {
+                    Text("Output unit")
+                }
+
+                Section {
+                    Text(convertedValue, format: .number)
+                }
+                header: {
+                    Text("Conversion from \(selectedInputUnit) to \(selectedOutputUnit)")
+                }
+            }
+            .navigationTitle("Unit Converter")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+
+                    Button("Done") {
+                        conversionIsFocused = false
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
