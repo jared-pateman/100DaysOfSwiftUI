d0b69f6f8b7c0b730cc5dbbd2dbb07ee8fb26577
Jared Pateman <jared.pateman@outlook.com>
2023-07-28 19:56:44 +0100

    Add initial project for GuessTheFlag project

---
+++ Projects/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Jared Pateman on 28/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
