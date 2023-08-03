68e5032bc5181f284fb41bebb71074f0fc0ee83e
Jared Pateman <jared.pateman@outlook.com>
2023-08-03 20:35:48 +0100

    Add initial BetterRest project

---
+++ Projects/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Jared Pateman on 03/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
