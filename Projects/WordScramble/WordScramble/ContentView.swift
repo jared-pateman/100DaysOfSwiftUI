b380ca89e28d36c028b25a9f993fcd7a4aa71a15
Jared Pateman <jared.pateman@outlook.com>
2023-08-06 21:06:21 +0100

    Add initial WordScramble project

---
+++ Projects/WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Jared Pateman on 06/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let people = ["Finn", "Leia", "Luke", "Rey"]
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Projects/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Jared Pateman on 06/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
