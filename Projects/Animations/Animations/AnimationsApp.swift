7b1fe45c8d879e498b720c5507393e5aa1977f8b
Jared Pateman <jared.pateman@outlook.com>
2023-08-09 20:07:30 +0100

    Add initial Animations project

---
+++ Projects/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Jared Pateman on 09/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
