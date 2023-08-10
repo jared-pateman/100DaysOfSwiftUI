cdfa1bd7405d370831c8f901d45847d505d3b808
Jared Pateman <jared.pateman@outlook.com>
2023-08-10 21:03:21 +0100

    Add animation examples to Animations project

--- Projects/Animations/Animations/ContentView.swift
+++ Projects/Animations/Animations/ContentView.swift
@@ -7,20 +7,107 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State private var animationAmount = 0.0
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
 
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading)
+        )
+    }
+}
+
+struct ContentView: View {
+    let letters = Array("Hello SwiftUI")
+    
+    @State private var animationAmount = 0.0
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+    @State private var hstackEnabled = false
+    @State private var hstackDragAmount = CGSize.zero
+    @State private var isShowingRed = false
+    
     var body: some View {
-        Button("Tap me") {
-            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
-                animationAmount += 360
+        VStack {
+            Button("Tap me") {
+                withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                    animationAmount += 360
+                }
             }
+            .padding(50)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+            
+            Button("Tap me") {
+                enabled.toggle()
+            }
+            .frame(width: 200, height: 200)
+            .background(enabled ? .blue : .red)
+            .animation(nil, value: enabled)
+            .foregroundColor(.white)
+            .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+            .animation(.interpolatingSpring(stiffness: 10, damping: 1), value: enabled)
+            
+            LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .frame(width: 300, height: 200)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .offset(dragAmount)
+                .gesture(
+                    DragGesture()
+                        .onChanged { dragAmount = $0.translation }
+                        .onEnded { _ in
+                            withAnimation(.spring()) {
+                                dragAmount = .zero
+                            }
+                        }
+                )
+            
+            HStack(spacing: 0) {
+                ForEach(0..<letters.count, id: \.self) { num in
+                    Text(String(letters[num]))
+                        .padding(5)
+                        .font(.title)
+                        .background(hstackEnabled ? .blue : .red)
+                        .offset(hstackDragAmount)
+                        .animation(.default.delay(Double(num) / 20), value: hstackDragAmount)
+                }
+            }
+            .gesture(
+                DragGesture()
+                    .onChanged { hstackDragAmount = $0.translation }
+                    .onEnded { _ in
+                        hstackDragAmount = .zero
+                        hstackEnabled.toggle()
+                    }
+            )
+            
+//            Button("Tap me") {
+//                withAnimation {
+//                    isShowingRed.toggle()
+//                }
+//            }
+//
+//            if isShowingRed {
+//                Rectangle()
+//                    .fill(.red)
+//                    .frame(width: 200, height: 200)
+//                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+                      // Use our newly created pivot transition.
+//                    .transition(.pivot)
+//            }
         }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
     }
 }
 
