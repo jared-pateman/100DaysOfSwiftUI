2d75333c70fade5e49b96015e58d54dc05581dea
Jared Pateman <jared.pateman@outlook.com>
2023-08-15 07:31:10 +0100

    Add initial iExpense project

---
+++ Projects/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Jared Pateman on 15/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
