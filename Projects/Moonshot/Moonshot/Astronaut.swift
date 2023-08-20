fb40197319571a78abd23caf900e4c9b6dc59c0c
Jared Pateman <jared.pateman@outlook.com>
2023-08-20 23:01:38 +0100

    Add initial functionality for Moonshot

---
+++ Projects/Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Jared Pateman on 20/08/2023.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
