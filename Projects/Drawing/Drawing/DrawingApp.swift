00dd933e05b398e2453257c1427640ca003d089b
Jared Pateman <jared.pateman@outlook.com>
2023-08-26 21:05:26 +0100

    Add initial Drawing project

---
+++ Projects/Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Jared Pateman on 26/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
