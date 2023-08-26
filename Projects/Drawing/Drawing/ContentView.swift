5fc0b851210283777c16ab4545e891d5a5ad27db
Jared Pateman <jared.pateman@outlook.com>
2023-08-26 22:03:32 +0100

    Add colorCyclingRectangle

--- Projects/Drawing/Drawing/ContentView.swift
+++ Projects/Drawing/Drawing/ContentView.swift
@@ -24,22 +24,70 @@ struct Arrow: Shape {
     }
 }
 
+struct ColorCyclingRectangle: View {
+    var amount = 0.0
+    var steps: Int = 100
+
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Rectangle()
+                    .inset(by: Double(value))
+                    .strokeBorder(LinearGradient(
+                        colors: [
+                            color(for: value, brightness: 1),
+                            color(for: value, brightness: 0.5)
+                        ],
+                        startPoint: .top,
+                        endPoint: .bottom
+                    ),
+                    lineWidth: 2)
+            }
+        }
+        .drawingGroup()
+    }
+    
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(steps) + amount
+        
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+        
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
 struct ContentView: View {
     @State private var lineThickness: Double = 5.0
+    @State private var colorCycle = 0.0
     
     var body: some View {
         VStack {
+            Text("Tap the arrow to change the line thickness")
+                .font(.title2)
+                .multilineTextAlignment(.center)
             Arrow()
                 .stroke(Color.red, style: StrokeStyle(lineWidth: CGFloat(lineThickness), lineCap: .round, lineJoin: .round))
                 .onTapGesture {
                     withAnimation {
-                        if lineThickness == 5.0 {
-                            lineThickness = 20.0
+                        if lineThickness < 25.0 {
+                            lineThickness += 5.0
                         } else {
                             lineThickness = 5.0
                         }
                     }
                 }
+            
+            Spacer()
+            
+            ColorCyclingRectangle(amount: colorCycle)
+                .padding()
+            
+            Text("Slide to change the rectangle")
+                .font(.title2)
+    
+            Slider(value: $colorCycle)
         }
         .padding()
     }
