888d24e346e205b594564cc771c8a2fe055da959
Jared Pateman <jared.pateman@outlook.com>
2023-09-03 20:13:53 +0100

    Add initial Bookworm project

---
+++ Projects/Bookwom/Bookwom/DataController.swift
@@ -0,0 +1,21 @@
+//
+//  DataController.swift
+//  Bookwom
+//
+//  Created by Jared Pateman on 03/09/2023.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "Bookworm")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+}
