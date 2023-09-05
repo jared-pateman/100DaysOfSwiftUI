3f706b6b80cfec6c7f66a61df50ee6da21223063
Jared Pateman <jared.pateman@outlook.com>
2023-09-05 20:35:44 +0100

    Add date to Books, and display in DetailView

--- Projects/Bookwom/Bookwom/DetailView.swift
+++ Projects/Bookwom/Bookwom/DetailView.swift
@@ -35,6 +35,12 @@ struct DetailView: View {
                 .font(.title)
                 .foregroundColor(.secondary)
             
+            if let date = book.date {
+                Text(date.formatted(date: .abbreviated, time: .omitted))
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+            
             Text(book.review ?? "No review")
                 .padding()
             
