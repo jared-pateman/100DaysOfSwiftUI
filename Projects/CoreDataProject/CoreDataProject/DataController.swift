7935de82c216893df397e90a82a6887b34d608f4
Jared Pateman <jared.pateman@outlook.com>
2023-09-06 20:54:58 +0100

    Add initial CoreDataProject project

---
+++ Projects/CoreDataProject/CoreDataProject/DataController.swift
@@ -0,0 +1,21 @@
+//
+//  DataController.swift
+//  CoreDataProject
+//
+//  Created by Jared Pateman on 06/09/2023.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "CoreDataProject")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+}
