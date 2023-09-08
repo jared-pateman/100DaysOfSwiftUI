61c3bfb0f0a3fe19dd163604003b41a6c1c766ec
Jared Pateman <jared.pateman@outlook.com>
2023-09-08 13:48:19 +0100

    Add initial workflow to CoreDataProject project

---
+++ Projects/CoreDataProject/CoreDataProject/FilteredList.swift
@@ -0,0 +1,22 @@
+//
+//  FilteredList.swift
+//  CoreDataProject
+//
+//  Created by Jared Pateman on 08/09/2023.
+//
+
+import SwiftUI
+
+struct FilteredList: View {
+    @FetchRequest var fetchRequest: FetchedResults<Singer>
+    
+    var body: some View {
+        List(fetchRequest, id: \.self) { singer in
+            Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
+        }
+    }
+    
+    init(filter: String) {
+        _fetchRequest = FetchRequest<Singer>(sortDescriptors: [], predicate: NSPredicate(format: "lastName BEGINSWITH %@", filter))
+    }
+}
---
+++ Projects/CoreDataProject/CoreDataProject/Singer+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Singer+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Jared Pateman on 08/09/2023.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Singer)
+public class Singer: NSManagedObject {
+
+}
