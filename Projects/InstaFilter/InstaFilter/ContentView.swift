1a4fdb7fb01df2df6c84dc2f4afbe256dd4a650a
Jared Pateman <jared.pateman@outlook.com>
2023-09-13 20:35:13 +0100

    Add initial helper structs for InstaFilter

--- Projects/InstaFilter/InstaFilter/ContentView.swift
+++ Projects/InstaFilter/InstaFilter/ContentView.swift
@@ -8,22 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var image: Image?
-    @State private var showingImagePicker = false
-    
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
-            
-            Button("Select Image") {
-                showingImagePicker = true
-            }
-        }
-        .sheet(isPresented: $showingImagePicker) {
-            ImagePicker()
-        }
+        Text("Hello, world!")
+            .padding()
     }
 }
 
