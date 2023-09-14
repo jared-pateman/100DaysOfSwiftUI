d8f0ba71862f9e399fd0da50cc2b6bb00657f8b9
Jared Pateman <jared.pateman@outlook.com>
2023-09-14 14:42:57 +0100

    Add select image functionality and make intensity slider functional

--- Projects/InstaFilter/InstaFilter/ContentView.swift
+++ Projects/InstaFilter/InstaFilter/ContentView.swift
@@ -5,11 +5,20 @@
 //  Created by Jared Pateman on 12/09/2023.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
     @State private var image: Image?
     @State private var filterIntensity = 0.5
+    
+    @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
+    
+    @State private var currentFilter = CIFilter.sepiaTone()
+    let context = CIContext()
+    
     var body: some View {
         NavigationView {
             VStack {
@@ -26,12 +35,13 @@ struct ContentView: View {
                         .scaledToFit()
                 }
                 .onTapGesture {
-                    // Select an image
+                    showingImagePicker = true
                 }
                 
                 HStack {
                     Text("Intensity")
                     Slider(value: $filterIntensity)
+                        .onChange(of: filterIntensity) { _ in applyProcessing() }
                 }
                 .padding(.vertical)
                 
@@ -47,11 +57,34 @@ struct ContentView: View {
             }
             .padding([.horizontal, .bottom])
             .navigationTitle("InstaFilter")
+            .onChange(of: inputImage) { _ in loadImage() }
+            .sheet(isPresented: $showingImagePicker) {
+                ImagePicker(image: $inputImage)
+            }
         }
+    }
+    
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+        
+        let beginImage = CIImage(image: inputImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
     }
     
     func save() {
         
+    }
+    
+    func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity)
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+        }
     }
 }
 
