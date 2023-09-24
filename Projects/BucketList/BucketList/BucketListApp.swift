e107db37a50853733d2ca30a19d8baa7aee908d7
Jared Pateman <jared.pateman@outlook.com>
2023-09-24 12:35:38 +0100

    Add initial BucketList project

---
+++ Projects/BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Jared Pateman on 24/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
