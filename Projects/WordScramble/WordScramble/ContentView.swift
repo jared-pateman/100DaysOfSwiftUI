25426bd1a7bb984f82ccbc7316e2203b2f05b562
Jared Pateman <jared.pateman@outlook.com>
2023-09-29 21:12:51 +0100

    Add accessibility labels to HStack to read word followed by letter count

--- Projects/WordScramble/WordScramble/ContentView.swift
+++ Projects/WordScramble/WordScramble/ContentView.swift
@@ -35,6 +35,8 @@ struct ContentView: View {
                             Image(systemName: "\(word.count).circle")
                             Text(word)
                         }
+                        .accessibilityElement()
+                        .accessibilityLabel("\(word), \(word.count) letters")
                     }
                 }
             }
