323e910039f92250b61892b57743b3b936e0e1bc
Jared Pateman <jared.pateman@outlook.com>
2023-10-12 14:56:21 +0100

    Add initial FlashZilla code

---
+++ Projects/FlashZilla/FlashZilla/FlashZillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashZillaApp.swift
+//  FlashZilla
+//
+//  Created by Jared Pateman on 12/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlashZillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
