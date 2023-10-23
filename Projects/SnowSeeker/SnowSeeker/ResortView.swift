71fa9bed4409ed2970397ff2649c62b0e14b22ba
Jared Pateman <jared.pateman@outlook.com>
2023-10-23 14:26:00 +0100

    Day 99 - Challenge 1: Add Image Credit to Resort View Image

--- Projects/SnowSeeker/SnowSeeker/ResortView.swift
+++ Projects/SnowSeeker/SnowSeeker/ResortView.swift
@@ -21,9 +21,16 @@ struct ResortView: View {
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 0) {
-                Image(decorative: resort.id)
-                    .resizable()
-                    .scaledToFit()
+                ZStack(alignment: .bottomTrailing) {
+                    Image(decorative: resort.id)
+                        .resizable()
+                        .scaledToFit()
+                    
+                    Text("Image credit: \(resort.imageCredit)")
+                        .padding(5)
+                        .foregroundColor(.white)
+                        .background(.black.opacity(0.5))
+                }
                 
                 HStack {
                     if sizeClass == .compact && typeSize > .large {
